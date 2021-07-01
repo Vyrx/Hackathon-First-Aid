e4cc17ab263464fdcc38fdba86fd784a8b7ad97a
Vyrx <58681073+Vyrx@users.noreply.github.com>
2021-07-01 23:01:34 +0800

    Update api.py

--- api.py
+++ api.py
@@ -61,8 +61,8 @@ import csv
 
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
+        print(event)
     print("!!!!!!!!!!!!!!!!!!!!!")
-    print(event)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text="bye")
